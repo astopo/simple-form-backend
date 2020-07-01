9af161d1464daf0e44b0d8cb491edd755f7c6807
astopo <amy.soetopo@gmail.com>
2020-07-01 11:09:53 -0700

    index.js - Setup express app, add middleware & controllers

---
+++ index.js
@@ -0,0 +1,18 @@
+const { PORT } = require('./env')
+
+const express = require('express')
+const app = express()
+
+// Middleware
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Routes
+const Controllers = require('./controllers')
+
+app.get('/', Controllers.Home)
+app.post('/', Controllers.FormHandler)
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
