59c4772bf9fc592c3bde93ba2646f00d9088c093
astopo <amy.soetopo@gmail.com>
2020-07-01 18:39:13 -0700

    Test: Helper tests

---
+++ test/helpers.test.js
@@ -0,0 +1,19 @@
+const { expect } = require('chai')
+
+describe('Helpers', function () {
+  describe('isEmailValid', function () {
+    it('should return false for an email that has an invalid domain', function () {})
+
+    it('should return false for an email that is missing the @ symbol', function () {})
+
+    it('should return true for an email that is valid', function () {})
+  })
+
+  describe('parseFormData', function () {
+    it('should throw an error if no form data is passed', function () {})
+
+    it('should return an empty array if an empty object is passed', function () {})
+
+    it('should return an array of strings compose of the keys and values of an object', function () {})
+  })
+})
