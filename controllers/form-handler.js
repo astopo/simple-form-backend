12688b19b2994c81b323070c3580d0a5da34e9a5
astopo <amy.soetopo@gmail.com>
2020-07-01 18:33:31 -0700

    Controllers: FormHandler - validate email, then parse form data, then send email

--- controllers/form-handler.js
+++ controllers/form-handler.js
@@ -1,20 +1,39 @@
+const { isEmailValid, parseFormData } = require('../helpers')
+const Email = require('../services/email')
+
 module.exports = function(req, res) {
   const email = req.params.email
 
+  const { subject } = req.query
+
   if (!email) {
-    // TODO - handle no email
+    return req.status(400).send({ error: 'Email required.' })
   }
 
-  // TODO - validate the email.
+  if (!isEmailValid(email)) {
+    return req.status(400).send({ error: 'Invalid email.' })
+  }
 
-  // TODO - grab form data.
   const data = req.body
 
   try {
-    const keys = Object.keys(data)
-    // TODO - build the email & send.
+    const stringArray = parseFormData(data)
+    // TODO - format for HTML
+
+    const emailOptions = {
+      to: email,
+      body: stringArray.join('\n')
+    }
+
+    const email = new Email({})
+
+    email.send()
+      .then(() => res.status(200).send({ message: 'OK' }))
+      .catch(error => res.status(500).send({ message: 'Failed to send email.', error: error.message }))
+
   } catch (error) {
     console.error('Failed to parse form data:', error)
-    res.status(500).send({ message: 'Invalid form data.' })
+
+    res.status(400).send({ error: 'Invalid form data.' })
   }
 }
