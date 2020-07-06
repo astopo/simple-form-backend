e0ed3f9745b0debacf03604fac52c94ca298237e
astopo <amy.soetopo@gmail.com>
2020-07-06 13:52:37 -0700

    Env: validate required env vars

--- env.js
+++ env.js
@@ -6,9 +6,18 @@ const DEFAULTS = {
   PORT: 3000
 }
 
+const REQUIRED = ['HONEYPOT_KEY', 'SENDGRID_API_KEY']
+
 // Apply defaults
 Object.keys(DEFAULTS).forEach(key => {
   ENV[key] = process.env[key] || DEFAULTS[key]
+})
+
+// Validate we have the required fields.
+REQUIRED.forEach(requiredKey => {
+  if (!ENV[requiredKey]) {
+    throw new Error(`Environment variable ${requiredKey} is required.`)
+  }
 })
 
 module.exports = ENV
